eaa44e9611e8ad10ff35ca974b1d6abca3d3b023
Giuliano Bertoti <giuliano.bertoti@gmail.com>
2016-03-14 11:08:27 -0300

    update

---
+++ Extras/app.js
@@ -0,0 +1,27 @@
+﻿var express = require('express');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+
+app.use(express.static('public'));
+
+app.get('/tablature/:band/:music', function (req, res) {
+  var ra = req.params.ra;
+  
+	MongoClient.connect('mongodb://localhost:27017/chords', function(err, db) {
+	  if (err) {
+		throw err;
+	  }
+		db.collection('tablatures').find({'band':band, 'music':music}).toArray(function(err, result) {
+			if (err) {
+				throw err;
+			}
+			res.send(result);
+		});
+	});
+  
+  
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
